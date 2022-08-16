dc17be5c7f14b3a416c28a88fd36b67ae6f6caff
yani27 <mohamed.youcefi.etu@univ-mosta.dz>
2022-08-16 16:02:08 +0100

    fix(header): header error

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-
+import  Header  from './components/header';
 
 export default function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([
+    { text: 'Learn React Native', key: '1' },
+  ])
   return (
     <View style={styles.container}>
+      <Header title="Todo :" />
       <View style={styles.subContainer}>
         <View style={styles.list}>
           <FlatList
             data={todos}
-            keyExtractor={(item) => item.id.toString()}
+            keyExtractor={(item) => item.key.toString()}
             renderItem={({ item, index }) => <Text key={index} style={styles.item}>{item.text}</Text>}
           />
         </View>
@@ -28,15 +31,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   subContainer: {
     flex: 1,
     padding: 40,
-    backgroundColor: '#111d5e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#111d5e',
+ 
   },
   list: {
     marginTop: 20,},
--- components/header.js
+++ components/header.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Header() {
+export default function Header({title}) {
     return (
         <View style={styles.header}>
-            <Text>
-                Todo :
+            <Text style={styles.title}>
+                {title}
             </Text>
         </View>
     )
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     header: {
         height: '10%',
         backgroundColor: '#111d5e',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    title:{
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold',
+
     }
 })
